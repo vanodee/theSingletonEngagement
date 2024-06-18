f67f46bdac16cc5c334d51f28cc91e133c102f60
vanodee <vanodee007@gmail.com>
2024-06-18 13:10:51 +0100

    FeedbackForm2 Validation and Loading State

--- src/Components/FeedbackForm2.jsx
+++ src/Components/FeedbackForm2.jsx
@@ -11,22 +11,31 @@ import {
     Text,
     Textarea
 } from '@chakra-ui/react'
+import { useState } from 'react'
 
 
 
 export default function FeedbackForm2() {
 
-    function submitForm(e) {
+    const [formWasSubmitted, setFormWasSubmitted] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+
+
+    const handleSubmit = (e) => {
         const formStuff = document.querySelector("form")
         e.preventDefault()
-        console.log("submitted")
         const formData = new FormData(formStuff)
+        setIsLoading(true)
+
         fetch("https://script.google.com/macros/s/AKfycbxndXOuddS0X_U6AwWrqbjXfzn8He9CTnuLsv2wVkVTPHezlycctXsBsOt1P4Uwu0LsnQ/exec", {
             method: 'POST',
             body: formData
+        }).then(() => {
+            console.log("submitted")
+            setIsLoading(false)
+            setFormWasSubmitted(true)
         })
     }
-
 
     //Custom Form Styling
     const inputFieldStyles = {
@@ -47,60 +56,102 @@ export default function FeedbackForm2() {
           w="100%"
           p="7%"
       >
-          <form
-              style={{ width: "100%" }}
-              onSubmit={(e)=>submitForm(e)}
-          >
-              <VStack spacing="3rem">
-                  <Heading
-                      color="white"
-                      fontSize="xxx-large"
-                  >
-                      FEEDBACK
-                  </Heading>
+          {!formWasSubmitted ? (
+            <>
+                <form
+                    style={{ width: "100%" }}
+                    onSubmit={(e)=>handleSubmit(e)}
+                >
+                    <VStack spacing="3rem">
+                        <Heading
+                            color="white"
+                            fontSize="xxx-large"
+                        >
+                            FEEDBACK
+                        </Heading>
 
-                  <Select
-                        id="gender"
-                        name="gender"
-                        //   onChange={formik.handleChange}
-                        //   value={formik.values.gender}
-                        //   onBlur={formik.handleBlur}
-                        placeholder="Tell us your gender"
-                          sx={inputFieldStyles}
-                      >
-                          <option value='Male'>Male</option>
-                          <option value='Female'>Female</option>
-                    </Select>
+                        <FormControl isRequired isDisabled={isLoading}>
+                            <Select
+                                id="gender"
+                                name="gender"
+                                placeholder="Tell us your gender"
+                                sx={inputFieldStyles}
+                            >
+                                <option value='Male'>Male</option>
+                                <option value='Female'>Female</option>
+                            </Select>
+                        </FormControl>
 
-                  <Textarea
-                          id="comment"
-                          name="comment"
-                          type="text"
-                          resize="none"
-                        //   onChange={formik.handleChange}
-                        //   value={formik.values.comment}
-                        //   onBlur={formik.handleBlur}
-                          placeholder="Tell us what you think"
-                          sx={inputFieldStyles}
-                      />
+                        <FormControl isRequired isDisabled={isLoading}>
+                            <Textarea
+                                id="comment"
+                                name="comment"
+                                type="text"
+                                resize="none"
+                                placeholder="Tell us what you think"
+                                sx={inputFieldStyles}
+                            />
+                        </FormControl>
 
-                  <Button
-                      variant="outline"
-                      size="lg"
-                      color="white"
-                      justifySelf="center"
-                      type="submit"
-                      w="100%"
-                      _hover={{
-                          color: "black",
-                          bg: "gray.200"
-                      }}
-                  >
-                      Submit
-                  </Button>
-              </VStack>
 
-          </form>
+                        <Button
+                            variant="outline"
+                            size="lg"
+                            color="white"
+                            justifySelf="center"
+                            type="submit"
+                            w="100%"
+                            isDisabled={isLoading}
+                            _hover={{
+                                color: "black",
+                                bg: "gray.200"
+                            }}
+                        >
+                            {!isLoading ? "Submit" : "Please Wait..."}
+                        </Button>
+                    </VStack>
+
+                </form>
+            </>
+          ) : (
+                    <>
+                        <VStack spacing="3rem">
+                          <Heading
+                              color="white"
+                              fontSize="xxx-large"
+                          >
+                              THANK YOU!
+                          </Heading>
+
+                          <Text
+                              color="white"
+                              fontSize="large"
+                              fontWeight="bold"
+                              textAlign="center"
+                          >
+                              Your Feedback was
+                              <br/>
+                              Submitted Successfully
+                          </Text>
+
+                          <Button
+                              variant="outline"
+                              size="lg"
+                              color="white"
+                              justifySelf="center"
+                              w="100%"
+                              onClick={()=>setFormWasSubmitted(false)}
+                              _hover={{
+                                  color: "black",
+                                  bg: "gray.200"
+                              }}
+                          >
+                              Done
+                          </Button>
+                        </VStack>
+                    </>
+        )}
+
       </VStack>
   )
 }
